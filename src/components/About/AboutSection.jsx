1d8a9c55b7d32053620531cac12bd8ad8eb18f95
Bruno Leite <bmarianoleite3@gmail.com>
2023-04-30 20:32:32 -0300

    changing team picture

--- src/components/About/AboutSection.jsx
+++ src/components/About/AboutSection.jsx
@@ -5,29 +5,28 @@ import { useTranslation } from "react-i18next";
 function AboutSection() {
   const { t } = useTranslation();
   return (
-    <div className="p-10 bg-white shadow-lg rounded-xl">
-      {/* Title */}
-      <div className="pt-10">
-        <p className="text-white text-[15px] text-center relative z-10 uppercase font-bold">
-          {t("aboutUs.title")}
-        </p>
-        <img
-          src={TitleBg}
-          alt="text_bg"
-          className="w-[200px] relative -top-[30px] left-[50%] -translate-x-[50%]"
-        />
-      </div>
-
-      <div className="flex min800:flex-col gap-20 items-center">
+    <div className="bg-gray text-white shadow-lg rounded-xl">
+      <div className="grid grid-cols-2 md1000:grid-cols-1 items-center">
         {/* text section */}
-        <div className="w-[60%] min800:w-full">
-          <h2 className="text-[3.4rem] font-bold min620:text-[2.5rem]">
+        <div className="p-10">
+          {/* Title */}
+          <div className="pt-10">
+            <p className="text-[15px] text-center relative z-10 uppercase font-bold">
+              {t("aboutUs.title")}
+            </p>
+            <img
+              src={TitleBg}
+              alt="text_bg"
+              className="w-[200px] relative -top-[30px] left-[50%] -translate-x-[50%]"
+            />
+          </div>
+          <h2 className="text-[3.4rem] text-center font-bold min620:text-[2.5rem]">
             {t("aboutUs.subtitle")}
           </h2>
-          <p className="text-[16px] text-[#646464] font-medium min800:text-justify">
+          <p className="text-[16px] font-medium text-justify">
             {t("aboutUs.description")}
           </p>
-          <div className="flex w-full min800:justify-center mt-10">
+          <div className="flex w-full justify-center mt-10">
             <MainButton
               color={`text-white`}
               bg={`bg-[#3f3f3f]`}
@@ -40,11 +39,11 @@ function AboutSection() {
           </div>
         </div>
         {/* img side */}
-        <div className="flex justify-end md1000:w-full md1000:px-5 md1000:justify-center">
+        <div className="flex justify-end h-full md1000:w-full md1000:justify-center">
           <img
             src="https://firebasestorage.googleapis.com/v0/b/dondoca-a6bd8.appspot.com/o/assets%2Fhome%2Fequipe.png?alt=media&token=b5ea3dfd-64b0-4b46-846a-cc6345e0ebb6"
             alt="brushes"
-            className="rounded-full w-full h-auto max-w-[45rem] min-w-[20rem] border-4 border-signature-gold"
+            className="rounded-xl"
           />
         </div>
       </div>
