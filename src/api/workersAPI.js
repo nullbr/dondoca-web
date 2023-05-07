f84abe4883dfb21621811cdf2e2e5f38082a3ade
Bruno Leite <bmarianoleite3@gmail.com>
2023-05-07 12:02:57 -0300

    fixing data fetching

--- src/api/workersAPI.js
+++ src/api/workersAPI.js
@@ -5,6 +5,6 @@ const WORKERS_URL = "workers";
 export async function fetchWorkers() {
   return axiosInstance
     .get(WORKERS_URL)
-    .then((response) => response.data.data)
+    .then((response) => response.data)
     .catch((error) => error.response.data);
 }
