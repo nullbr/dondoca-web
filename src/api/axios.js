a53c630877cbcdd88861357375f64a568ed55f2d
Bruno Leite <bmarianoleite3@gmail.com>
2023-05-10 09:43:19 -0300

    adding Client id to request headers

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,6 +4,7 @@ const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
+    Client: import.meta.env.VITE_CLIENT_ID,
     withCredentials: true,
   },
 });
